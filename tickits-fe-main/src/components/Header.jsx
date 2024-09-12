f7210c1f0b2385648d5e0ca72074b665bb7c384e
seasonpaudel <paudelseason@gmail.com>
2024-09-12 19:10:49 +0545

    jakarta to pokhara

--- tickits-fe-main/src/components/Header.jsx
+++ tickits-fe-main/src/components/Header.jsx
@@ -389,12 +389,13 @@ function Header() {
 export async function getStaticProps() {
   const locations = [
     {
-      key: "cgv-jaksel",
-      title: "CGV Jakarta Selatan",
+      key: "midtown-cinemas-pokhara"
+      title: "Midtown Cinemas Pokhara",
     },
     {
-      key: "cgv-pvj-bandung",
-      title: "CGV Paris Van Java Bandung",
+      key: "qfx-cinemas-kathmandu",
+      title: "QFX Cinemas Kathmandu",
+      
     },
   ];
 
